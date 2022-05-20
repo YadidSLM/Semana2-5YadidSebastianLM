a9cefb9f2d1d32533a71dc183a8f90d9ddd007f8
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2022-05-20 01:47:11 -0500

    Se añadió la opción de otra con un nuevo input al dar click en ella y principios de borrar tarea y marcar como completada

--- Lista_de_tareas/dynamics/js/js.js
+++ Lista_de_tareas/dynamics/js/js.js
@@ -2,31 +2,47 @@ const titulo = document.getElementById("titulo");
 const materia = document.getElementById("materia");
 const anadir = document.getElementById("anadir");
 const tareas = document.getElementById("tareas");
+const otraMateria = document.getElementById("otraMateria");
 
-
+materia.addEventListener("click", (evento) =>{
+    if(evento.target.value != "Otra")
+    {
+        otraMateria.style.display = "none";
+    }
+    else
+    {
+        console.log("Otra");
+        otraMateria.style.display = "inline";
+    }
+});
 anadir.addEventListener("click", (evento) =>{
     if(titulo.value != '')
     {
         console.log(tareas.innerHTML);
         console.log(tareas.outerHTML);
-        //console.log(evento.target.innerHTML);
-        //tareas.innerHTML = tareas.outerHTML + tareas.innerHTML + titulo.value + ' ' + materia.value;
-        //tareas.style.alignSelf = "center";
-        tareas.innerHTML = tareas.innerHTML + '<br/>' + '<div class="tar">'+ '<button id="marcar">Marcar como acabada</button>' + '<button id="borrar">Eliminar</button>' + materia.value  + ': ' + titulo.value + '</div>';
+        if(otraMateria.style.display == "inline") // y que no se repite la materia)
+        {
+            materia.innerHTML += "<option value='" + otraMateria.value +"'>" + otraMateria.value + '</option>';    
+            otraMateria.style.display = "none";
+            materia.value = otraMateria.value;
+        }
+        tareas.innerHTML = tareas.innerHTML + '<br/>' + '<div class="tar">'+ '<button id="marcar">Marcar como acabada</button>' + '<span> </span>' + '<button id="borrar">Eliminar</button>' + materia.value  + ': ' + titulo.value + '</div>';
     }
 });
 tareas.addEventListener("click", (evento) =>{
-    if(evento.target.getElementById = "borrar")
+    if(evento.target.getElementById == "marcar")
     {
+        const marcar = document.getElementById("marcar");
+        marcar.style.backgroundColor = "blue";
+        console.log("Completada");
+    }
+    if(evento.target.getElementById == "borrar")
+    {
+        console.log("Borrar");
         evento.target.parentElement.outerHTML = '';
-        console.log(evento.target.parentElement.outerHTML);
-        console.log(evento.target.parentElement);
-        console.log(evento.target);
-        console.log(evento);
+        // console.log(evento.target.parentElement.outerHTML);
+        // console.log(evento.target.parentElement);
+        // console.log(evento.target);
+        // console.log(evento);
     }
-    else if(evento.target.getElementById = "marca")
-    {
-        evento.target.style.color = "blue";
-        
-    }
-})
+});
