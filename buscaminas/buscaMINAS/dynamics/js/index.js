7ed1e314df34a15abbc6f70327a96e9c24a397c6
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2022-05-20 01:52:11 -0500

    buscaminas

--- buscaminas/buscaMINAS/dynamics/js/index.js
+++ buscaminas/buscaMINAS/dynamics/js/index.js
@@ -1,19 +1,23 @@
 const click1 = document.getElementById("uno");
 const click2 = document.getElementById("dos");
+const registro = document.getElementById("registro");
 var tiempo1;
 var tiempo2;
 var duracion;
-fecha = new Date();
+var fecha = new Date();
 console.log(fecha);
 
 click1.addEventListener("click", (evento) =>{
     tiempo1 = fecha.getTime();
     console.log(fecha.getTime());
-})
+});
 
 click2.addEventListener("click", (evento) =>{
+    fecha = new Date();
     tiempo2 = fecha.getTime();
     console.log(fecha.getTime());
+    console.log(fecha);
     duracion = tiempo2 - tiempo1;
     console.log('duracion:' + duracion);
-})
+    registro.innerHTML = "<p>" + duracion + "</p>";
+});
