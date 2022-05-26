c4a8f842ca48a33b9f0f47c760fe7dc5daedd44c
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2022-05-25 20:18:53 -0500

    Temporizador segundos cuando minutos son 0 funcional

--- Estrella/dynamics/estrella.js
+++ Estrella/dynamics/estrella.js
@@ -6,12 +6,64 @@ const relleno = document.getElementById("relleno");
 const nPicos = document.getElementById("nPicos");
 const picudez = document.getElementById("picudez");
 
-let longitudLinea = picudez.value;
-let anguloGrados = 360 / nPicos.value;
-let anguloRadianes = (anguloGrados * Math.PI) / 180;
-let despX = 100 * Math.cos(anguloRadianes);
-let despY = 100 * Math.sin(anguloRadianes);
+// let longitudLinea = 50;
+// // let anguloGrados = 360 / nPicos.value;
+// let anguloGrados = 72;
+// let anguloRadianes = (anguloGrados * Math.PI) / 180;
+// let despX = longitudLinea * Math.cos(anguloRadianes);
+// let despY = longitudLinea * Math.sin(anguloRadianes);
 
+let longitudLinea = 50;
+//let anguloGrados = 360 / nPicos.value;
+let anguloGrados = nPicos.value;
+let anguloRadianes = (anguloGrados * Math.PI) / 180;
+let despX = longitudLinea * Math.cos(anguloRadianes);
+let despY = longitudLinea * Math.sin(anguloRadianes);
+
+// function valInicial()
+// {
+//     let longitudLinea = 50;
+//     // let anguloGrados = 360 / nPicos.value;
+//     let anguloGrados = 72;
+//     let anguloRadianes = (anguloGrados * Math.PI) / 180;
+//     let despX = longitudLinea * Math.cos(anguloRadianes);
+//     let despY = longitudLinea * Math.sin(anguloRadianes);    
+// }
+// function dibujar()
+// {
+//     ctx.beginPath();
+
+//     ctx.moveTo(150, 150);
+//     ctx.lineTo(150 + despX, 150 + despY);
+
+//     anguloGrados = 72 * 2;
+//     anguloRadianes = (anguloGrados * Math.PI) / 180;
+//     despX = longitudLinea * Math.cos(anguloRadianes);
+//     despY = longitudLinea * Math.sin(anguloRadianes);
+//     ctx.lineTo(150 + despX, 150 + despY);
+
+//     anguloGrados = 72 * 3;
+//     anguloRadianes = (anguloGrados * Math.PI) / 180;
+//     despX = longitudLinea * Math.cos(anguloRadianes);
+//     despY = longitudLinea * Math.sin(anguloRadianes);
+//     ctx.lineTo(150 + despX, 150 + despY);
+
+//     anguloGrados = 72 * 4;
+//     anguloRadianes = (anguloGrados * Math.PI) / 180;
+//     despX = longitudLinea * Math.cos(anguloRadianes);
+//     despY = longitudLinea * Math.sin(anguloRadianes);
+//     ctx.lineTo(150 + despX, 150 + despY);
+
+//     anguloGrados = 72 * 5;
+//     anguloRadianes = (anguloGrados * Math.PI) / 180;
+//     despX = longitudLinea * Math.cos(anguloRadianes);
+//     despY = longitudLinea * Math.sin(anguloRadianes);
+//     ctx.lineTo(150 + despX, 150 + despY);
+//     ctx.stroke();
+    
+//     ctx.closePath();
+// }
+// dibujar();
 
 function dibujar()
 {
@@ -24,7 +76,7 @@ function dibujar()
     ctx.closePath();
 }
 
-nPicos.addEventListener("focusout", ()=>{
+nPicos.addEventListener("changet", ()=>{
     console.log(nPicos.value);
     longitudLinea = picudez.value;
     anguloGrados = 360 / nPicos.value;
--- Temporizador/dynamics/tempo.js
+++ Temporizador/dynamics/tempo.js
@@ -3,31 +3,97 @@ const parar = document.getElementById("parar");
 const registro = document.getElementById("registro");
 const minutos = document.getElementById("minutos");
 const segundos = document.getElementById("segundos");
-
+//==================== Del tiempo indicado a cero (Funciona los segundos cuando los minutos son 0)======================
+let tempo;
 let tempoS;
 let tempoM;
-let seg = 0;
-let min = 0;
+let seg = segundos.value;
+let min = minutos.value;
+let passedS = 0;
+let passedM = 0;
+let pase = 0;
 
 iniciar.addEventListener("click", (evento) =>{
-    tempoM = setInterval(()=>{
-        min++;
-        registro.innerHTML += "<p>" + min + " " + ":" + " "+"</p>";
-        if(min == 59)
-        {
-            min = 0;
-        }
-    }, minutos)
-    tempoS = setInterval(()=>{
-        seg++;
-        console.log(segundos);
-        registro.innerHTML += "<p>" + seg +"</p>";
-        if(seg == 59)
-        {
-            seg = 0;
-        }
-    }, segundos)
+    if(minutos.value != "" && segundos.value != "" && pase < 2)
+    {
+        pase++;
+        min = minutos.value;
+        seg = segundos.value;
+        tempoS = setInterval(()=>{
+            seg--;
+            console.log(min + ":" + seg);
+            if(seg == 0 && min == 0)
+            {
+                seg = 0;
+                clearInterval(tempoS);
+                clearInterval(tempoM);
+                alert("Ring ring ha terminado el tiempo");
+            }
+            else if(min > 0)
+            {
+                if(seg == 0)
+                {
+                    min--;
+                }
+                seg = 59;
+            }
+            registro.innerHTML = " ";
+            registro.innerHTML += "<p>" + min + " " + ":" + " "+ seg + "</p>";
+        }, 1000)
+    }
+    
 });
 parar.addEventListener("click", (evento)=>{
     clearInterval(tempoS);
+    clearInterval(tempoM);
+
 });
+
+//====================== De cero al número indicado ==============================
+
+// let tempo;
+// let tempoS;
+// let tempoM;
+// let seg = 0;
+// let min = 0;
+// let passedS = 0;
+// let passedM = 0;
+
+// iniciar.addEventListener("click", (evento) =>{
+//     if(minutos.value != "" && segundos.value != "")
+//     {
+//         tempoS = setInterval(()=>{
+//             seg++;
+//             console.log(segundos);
+//             if(seg == 59)
+//             {
+//                 seg = 0;
+//                 passedS++;
+//                 if(passedS == segundos.value)
+//                 {
+//                     clearInterval(tempoS);
+//                     clearInterval(tempoM);
+//                 }
+//             }
+//         }, 1000)
+//         tempoM = setInterval(()=>{
+//             min++;
+//             if(min == 59)
+//             {
+//                 min = 0;
+//             }
+//         }, 60000)
+//         tempo = setInterval(()=>{
+//             registro.innerHTML += "<p>" + min + " " + ":" + " "+ seg + "</p>";
+//             registro.innerHTML = " ";
+//             registro.innerHTML += "<p>" + min + " " + ":" + " "+ seg + "</p>";
+
+//         }, 10000)
+//     }
+    
+// });
+// parar.addEventListener("click", (evento)=>{
+//     clearInterval(tempoS);
+//     clearInterval(tempoM);
+
+// });
