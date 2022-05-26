b0bcb5b766c98e7586a178238e9bbaa655fadde3
YadidSLM <yadid_sebastian@comunidad.unam.mx>
2022-05-26 01:55:52 -0500

    Audio incluido

--- Temporizador/dynamics/tempo.js
+++ Temporizador/dynamics/tempo.js
@@ -3,7 +3,9 @@ const parar = document.getElementById("parar");
 const registro = document.getElementById("registro");
 const minutos = document.getElementById("minutos");
 const segundos = document.getElementById("segundos");
-//==================== Del tiempo indicado a cero (Funciona los segundos cuando los minutos son 0)======================
+//Toma como referencia el index.html
+const audio  = new Audio("./statics/Audio/despertar.mp3");
+//==================== Del tiempo indicado a cero ======================
 let tempo;
 let tempoS;
 let tempoM;
@@ -12,6 +14,7 @@ let min = minutos.value;
 let passedS = 0;
 let passedM = 0;
 let pase = 0;
+let aceptar;
 
 iniciar.addEventListener("click", (evento) =>{
     if(minutos.value != "" && segundos.value != "" && pase < 2 && minutos.value >= 0 && segundos.value >= 0)
@@ -28,7 +31,20 @@ iniciar.addEventListener("click", (evento) =>{
                 seg = 0;
                 clearInterval(tempoS);
                 clearInterval(tempoM);
-                alert("Ring ring ha terminado el tiempo");
+                audio.volume = 0.1;
+                audio.play();
+                audio.loop = true;
+                aceptar = confirm("Kikiriki ha terminado el tiempo");
+                if(aceptar == false)
+                {
+                    audio.volume = 0.1;
+                    audio.play();
+                    audio.loop = true;
+                }
+                else
+                {
+                    audio.loop = false;
+                }
             }
             else if(seg == 0)
             {
@@ -49,54 +65,18 @@ iniciar.addEventListener("click", (evento) =>{
 parar.addEventListener("click", (evento)=>{
     clearInterval(tempoS);
     clearInterval(tempoM);
-
+    if(seg == 0 && min == 0)
+    {
+        aceptar = confirm("Kikiriki ha terminado el tiempo");
+        if(aceptar == false)
+        {
+            audio.volume = 0.1;
+            audio.play();
+            audio.loop = true;
+        }
+        else
+        {
+            audio.loop = false;
+        }
+    }
 });
-
-//====================== De cero al número indicado ==============================
-
-// let tempo;
-// let tempoS;
-// let tempoM;
-// let seg = 0;
-// let min = 0;
-// let passedS = 0;
-// let passedM = 0;
-
-// iniciar.addEventListener("click", (evento) =>{
-//     if(minutos.value != "" && segundos.value != "")
-//     {
-//         tempoS = setInterval(()=>{
-//             seg++;
-//             console.log(segundos);
-//             if(seg == 59)
-//             {
-//                 seg = 0;
-//                 passedS++;
-//                 if(passedS == segundos.value)
-//                 {
-//                     clearInterval(tempoS);
-//                     clearInterval(tempoM);
-//                 }
-//             }
-//         }, 1000)
-//         tempoM = setInterval(()=>{
-//             min++;
-//             if(min == 59)
-//             {
-//                 min = 0;
-//             }
-//         }, 60000)
-//         tempo = setInterval(()=>{
-//             registro.innerHTML += "<p>" + min + " " + ":" + " "+ seg + "</p>";
-//             registro.innerHTML = " ";
-//             registro.innerHTML += "<p>" + min + " " + ":" + " "+ seg + "</p>";
-
-//         }, 10000)
-//     }
-    
-// });
-// parar.addEventListener("click", (evento)=>{
-//     clearInterval(tempoS);
-//     clearInterval(tempoM);
-
-// });
